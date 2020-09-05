db157f2daeb88d923ed22f39c801600baf2351f2
Хитров Александр <khitrov.av@erkapharm.com>
2020-09-05 19:11:35 +0300

    Slide form MVP

--- resources/views/slides.blade.php
+++ resources/views/slides.blade.php
@@ -19,37 +19,42 @@
                         <div class="form-row">
                             <div class="form-group col-md-6">
                                 <label for="name">Название</label>
-                                <input type="text" class="form-control" id="name">
+                                <input type="text" class="form-control" id="name" value="Первый слайд" required>
                             </div>
                             <div class="form-group col-md-3">
                                 <label for="from">От</label>
-                                <input type="datetime-local" class="form-control" id="from">
+                                <input type="datetime-local" class="form-control" id="from" value="@php echo date('Y-m-d\TH:i', strtotime(now())); @endphp" required>
                             </div>
                             <div class="form-group col-md-3">
                                 <label for="to">До</label>
-                                <input type="datetime-local" class="form-control" id="to">
+                                <input type="datetime-local" class="form-control" id="to" value="@php echo date('Y-m-d\TH:i', strtotime('+30 days')); @endphp" required>
                             </div>
                         </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-4">
+                        <div class="form-row d-flex align-items-end">
+                            <div class="form-group col-md-2">
                                 <label for="resolution">Разрешение</label>
                                 <select class="custom-select" id="resolution" required>
-                                    <option selected disabled value="">Выберите...</option>
                                     <option>1920x1024</option>
                                     <option>768x1024</option>
                                 </select>
                             </div>
-                            <div class="form-group col-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="default">
-                                    <label class="form-check-label" for="default">По-умолчанию</label>
-                                </div>
+                            <div class="form-group col-md-2">
+                                <label for="name">Время отображения, сек.</label>
+                                <input type="text" class="form-control" id="name" value="3" required>
                             </div>
-                            <div class="form-group col-md-4">
-                                <div class="form-check">
-                                <input class="form-check-input" type="checkbox" id="active">
+                            <div class="form-group col-md-8">
+                                <label for="file" class="custom-file-label">Файл</label>
+                                <input type="file" class="custom-file-input form-control" id="file">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" id="default">
+                                <label class="form-check-label" for="default">По-умолчанию</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="checkbox" id="active" checked>
                                 <label class="form-check-label" for="active">Активный</label>
-                                </div>
                             </div>
                         </div>
                     </form>
